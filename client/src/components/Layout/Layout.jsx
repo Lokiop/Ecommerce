1279a606b7a6dc4074af474990f053f4bced1b7f
Darshan <darshanheda08@gmail.com>
2023-09-20 21:18:56 +0530

    SEO (Search Engine Optmization)

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Helmet } from "react-helmet";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title, description, author, keywords }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="author" content={author} />
+        <meta name="keywords" content={keywords} />
+        <title>{title}</title>
+      </Helmet>
       <Header />
       <main style={{ minHeight: "75vh" }}>{children}</main>
       <Footer />
     </div>
   );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce App",
+  description: "Mern Stack based Ecommerce App",
+  author: "Darshan Heda",
+  keywords: "mern, react, node, mongodb, express",
 };
 
 export default Layout;
